40eb4599fd722fbed472949f4931a27aab4dc760
nieo1205 <damho.11081205@gmail.com>
2026-03-19 19:14:58 +0900

    script.js
    Signed-off-by: nieo1205 <damho.11081205@gmail.com>

--- script.js
+++ script.js
@@ -1,82 +1,31 @@
-const PASSWORD = "7586";
-
-function login(){
-
-const name = document.getElementById("name").value;
-const password = document.getElementById("password").value;
-const message = document.getElementById("message");
-
-if(name === ""){
-message.innerText = "이름을 입력하세요";
-return;
+function showIntro(){
+document.getElementById("startContainer").style.display = "none";
+document.getElementById("introContainer").style.display = "block";
 }
 
-if(password !== PASSWORD){
-message.innerText = "비밀번호가 틀렸습니다";
-return;
-}
-
-message.innerText = "";
-
-document.getElementById("loginContainer").style.display = "none";
+function goToMain(){
+document.getElementById("introContainer").style.display = "none";
 document.getElementById("choiceContainer").style.display = "block";
-
 }
 
-
+// 영상 재생
 function playVideo(id){
-
 document.getElementById("choiceContainer").style.display = "none";
 
 const player = document.getElementById("ytPlayer");
 player.src = "https://www.youtube.com/embed/" + id + "?autoplay=1";
 
 document.getElementById("videoScreen").style.display = "block";
-
 }
 
-
+// 디스코드
 function joinDiscord(){
-
 window.open("https://discord.gg/GtXZMKSAaf","_blank");
-
 }
 
-
-/* 뒤로가기 버튼 */
-
+// 뒤로가기
 function goBack(){
-
 document.getElementById("videoScreen").style.display = "none";
 document.getElementById("choiceContainer").style.display = "block";
-
 document.getElementById("ytPlayer").src = "";
-
-}
-
-
-/* 엔터키 로그인 */
-
-document.getElementById("password").addEventListener("keydown", function(e){
-
-if(e.key === "Enter"){
-login();
-}
-
-});
-
-function generateNickname(){
-
-const words = [
-"Shadow","Nova","Racer","Ghost","Storm",
-"Blaze","Phantom","Zero","Drift","Turbo"
-];
-
-const randomWord = words[Math.floor(Math.random()*words.length)];
-const randomNumber = Math.floor(Math.random()*10000);
-
-const nickname = randomWord + randomNumber;
-
-document.getElementById("name").value = nickname;
-
 }
